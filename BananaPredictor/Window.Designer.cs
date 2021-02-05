210c00a0098998080c0497e8db9f8abbfdce3682
Kevinbestkiller KBK <kevinbestkiller@gmail.com>
2021-02-05 18:21:21 +0100

    Many graphical updates for better design

--- BananaPredictor/Window.Designer.cs
+++ BananaPredictor/Window.Designer.cs
@@ -36,11 +36,16 @@ namespace BananaPredictor
             this.tbBeatmap = new System.Windows.Forms.TextBox();
             this.cbDebug = new System.Windows.Forms.CheckBox();
             this.ttDebug = new System.Windows.Forms.ToolTip(this.components);
+            this.pTop = new System.Windows.Forms.Panel();
+            this.wbMini = new System.Windows.Forms.Button();
+            this.wbExit = new System.Windows.Forms.Button();
+            this.tAnimation = new System.Windows.Forms.Timer(this.components);
+            this.pTop.SuspendLayout();
             this.SuspendLayout();
             // 
             // submitBeatmap
             // 
-            this.submitBeatmap.Location = new System.Drawing.Point(321, 55);
+            this.submitBeatmap.Location = new System.Drawing.Point(321, 92);
             this.submitBeatmap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.submitBeatmap.Name = "submitBeatmap";
             this.submitBeatmap.Size = new System.Drawing.Size(86, 31);
@@ -51,7 +56,7 @@ namespace BananaPredictor
             // 
             // browseBeatmap
             // 
-            this.browseBeatmap.Location = new System.Drawing.Point(321, 16);
+            this.browseBeatmap.Location = new System.Drawing.Point(321, 53);
             this.browseBeatmap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.browseBeatmap.Name = "browseBeatmap";
             this.browseBeatmap.Size = new System.Drawing.Size(86, 31);
@@ -62,7 +67,7 @@ namespace BananaPredictor
             // 
             // tbBeatmap
             // 
-            this.tbBeatmap.Location = new System.Drawing.Point(14, 16);
+            this.tbBeatmap.Location = new System.Drawing.Point(12, 53);
             this.tbBeatmap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tbBeatmap.Name = "tbBeatmap";
             this.tbBeatmap.Size = new System.Drawing.Size(300, 27);
@@ -71,7 +76,8 @@ namespace BananaPredictor
             // cbDebug
             // 
             this.cbDebug.AutoSize = true;
-            this.cbDebug.Location = new System.Drawing.Point(13, 55);
+            this.cbDebug.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.cbDebug.Location = new System.Drawing.Point(12, 96);
             this.cbDebug.Name = "cbDebug";
             this.cbDebug.Size = new System.Drawing.Size(119, 24);
             this.cbDebug.TabIndex = 3;
@@ -79,20 +85,72 @@ namespace BananaPredictor
             this.cbDebug.UseVisualStyleBackColor = true;
             this.cbDebug.MouseHover += new System.EventHandler(this.cbDebug_MouseHover);
             // 
+            // pTop
+            // 
+            this.pTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.pTop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pTop.Controls.Add(this.wbMini);
+            this.pTop.Controls.Add(this.wbExit);
+            this.pTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pTop.Location = new System.Drawing.Point(0, 0);
+            this.pTop.Name = "pTop";
+            this.pTop.Size = new System.Drawing.Size(421, 35);
+            this.pTop.TabIndex = 4;
+            this.pTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tPanel_MouseDown);
+            // 
+            // wbMini
+            // 
+            this.wbMini.BackColor = System.Drawing.Color.Transparent;
+            this.wbMini.Dock = System.Windows.Forms.DockStyle.Right;
+            this.wbMini.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.wbMini.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.wbMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.wbMini.ForeColor = System.Drawing.SystemColors.Control;
+            this.wbMini.Location = new System.Drawing.Point(305, 0);
+            this.wbMini.Name = "wbMini";
+            this.wbMini.Size = new System.Drawing.Size(57, 33);
+            this.wbMini.TabIndex = 6;
+            this.wbMini.Text = "_";
+            this.wbMini.UseVisualStyleBackColor = false;
+            this.wbMini.Click += new System.EventHandler(this.wbMini_Click);
+            // 
+            // wbExit
+            // 
+            this.wbExit.BackColor = System.Drawing.Color.Transparent;
+            this.wbExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.wbExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.wbExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.wbExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.wbExit.ForeColor = System.Drawing.SystemColors.Control;
+            this.wbExit.Location = new System.Drawing.Point(362, 0);
+            this.wbExit.Name = "wbExit";
+            this.wbExit.Size = new System.Drawing.Size(57, 33);
+            this.wbExit.TabIndex = 5;
+            this.wbExit.Text = "X";
+            this.wbExit.UseVisualStyleBackColor = false;
+            this.wbExit.Click += new System.EventHandler(this.wbExit_Click);
+            // 
+            // tAnimation
+            // 
+            this.tAnimation.Tick += new System.EventHandler(this.tAnimation_Tick);
+            // 
             // K3Banana
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 105);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(421, 136);
+            this.Controls.Add(this.pTop);
             this.Controls.Add(this.cbDebug);
             this.Controls.Add(this.tbBeatmap);
             this.Controls.Add(this.browseBeatmap);
             this.Controls.Add(this.submitBeatmap);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "K3Banana";
             this.Text = "Banana Predictor";
+            this.pTop.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +163,10 @@ namespace BananaPredictor
         private System.Windows.Forms.TextBox tbBeatmap;
         private System.Windows.Forms.CheckBox cbDebug;
         private System.Windows.Forms.ToolTip ttDebug;
+        private System.Windows.Forms.Panel pTop;
+        private System.Windows.Forms.Button wbExit;
+        private System.Windows.Forms.Button wbMini;
+        private System.Windows.Forms.Timer tAnimation;
     }
 }
 
--- BananaPredictor/Window.cs
+++ BananaPredictor/Window.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 using BananaPredictor.Osu;
@@ -9,16 +11,31 @@ using BananaPredictor.Osu;
 namespace BananaPredictor
 {
     // Apologies for the terrible code/not using other libraries such as OsuPrasers to get the job done
-    // TODO: Finish development with the spin maker
     public partial class K3Banana : Form
     {
+        // For Dialog Box
         private readonly OpenFileDialog fileDir = new();
+        
+        // For Window
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        // Window Related Animations
+        int OriginalHeight, OriginalWidth;
+        int ScreenHeight = Screen.PrimaryScreen.Bounds.Height, ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
 
         public K3Banana()
         {
             InitializeComponent();
+            OriginalWidth = this.Width;
+            OriginalHeight = this.Height;
         }
 
+        // Main Window
         private void SubmitBeatmap_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Debug.WriteLine("Submission Clicked");
@@ -32,11 +49,11 @@ namespace BananaPredictor
             bool did;
             if (cbDebug.Checked)
             {
-                var aNew = new BananaSpinPredictor();
+                BananaSpinPredictor aNew = new();
                 did = aNew.SpinnerPredictor(tbBeatmap.Text);
             } else
             {
-                var aNew = new BananaSpinMaker();
+                BananaSpinMaker aNew = new();
                 did = aNew.SpinnerMaker(tbBeatmap.Text);
                 MessageBox.Show("Currently under development.");
             }
@@ -63,13 +80,13 @@ namespace BananaPredictor
                 tbBeatmap.Text = fileDir.FileName;
                 return;
             }
-            else if (fileDir.ShowDialog().Equals(DialogResult.Cancel)) // TODO: FIX LATER
+            else if (fileDir.ShowDialog().Equals(DialogResult.Cancel)) // TODO: Fix issue where it opens again
             {
                 return;
             }
             else
             {
-                MessageBox.Show("An error has occurred. Make sure you selected the correct file.", "Error"); // TODO: FIX LATER
+                MessageBox.Show("An error has occurred. Make sure you selected the correct file.", "Error");
                 return;
             }
         }
@@ -77,6 +94,43 @@ namespace BananaPredictor
         private void cbDebug_MouseHover(object sender, EventArgs e)
         {
             ttDebug.Show("Using Predictor than Maker to test and see if the code works under other specific unknown situations", cbDebug);
+        }
+
+        // Window Panel/Top Bar
+        private void tPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void wbMini_Click(object sender, EventArgs e)
+        {
+            tAnimation.Interval = 15;
+            tAnimation.Start();
+        }
+
+        private void wbExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tAnimation_Tick(object sender, EventArgs e)
+        {
+            if (this.Height > 5 && this.Width > 5)
+            {
+                this.Height -= OriginalHeight / 10;
+                this.Width -= OriginalHeight / 10;
+            }
+            else
+            {
+                tAnimation.Stop();
+                this.WindowState = FormWindowState.Minimized;
+                this.Height = OriginalHeight;
+                this.Width = OriginalWidth;
+            }
         }
     }
 }
