a5610ce9b6c0e9c598bb751361987bd084155a64
Kevinbestkiller KBK <kevinbestkiller@gmail.com>
2021-02-05 18:20:53 +0100

    It's own file for better readability and less bloat

---
+++ BananaPredictor/Osu/PutTogether.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BananaPredictor.Osu
+{
+    public class PutTogether
+    {
+        public String PutLineTogether(int bmNumber, IEnumerable<String> lines) => String.Join("", lines.Skip(bmNumber).Take(1).First().Split(':').Skip(1));
+    }
+}
