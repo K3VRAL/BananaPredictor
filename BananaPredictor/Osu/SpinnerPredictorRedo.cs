16e5ab6dddcf2189eba890f7d115db147ebbe496
Kevinbestkiller KBK <kevinbestkiller@gmail.com>
2021-03-03 17:02:34 +0100

    Made alternative/testing file for further tests and such

--- BananaPredictor/Osu/SpinnerPredictorRedo.cs
+++ BananaPredictor/Osu/SpinnerPredictorRedo.cs
@@ -7,11 +7,14 @@ using osu.Game.Rulesets.Catch.MathUtils;
 using osu.Game.Rulesets.Catch.UI;
 using osu.Game.Rulesets.Catch.Beatmaps;
 using osu.Game.Screens.Play;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Migrations;
+using System.CodeDom.Compiler;
 
 namespace BananaPredictor.Osu
 {
     // Used for debugging if program/initial logic works
-    public class BananaSpinPredictor
+    public class BananaSpinPredictorRedo
     {
         public bool SpinnerPredictor(string path, bool debugging, int startPoint, int endPoint)
         {
@@ -21,6 +24,7 @@ namespace BananaPredictor.Osu
             // Map info lines (Used for the file name and such)
             GetMusicInfo MusicInfo = new();
             int bmHitObjects = MusicInfo.GetItemLine("[HitObjects]", lines);
+            bmHitObjects++;
 
             // If not found any
             if (bmHitObjects < 0)
@@ -40,23 +44,23 @@ namespace BananaPredictor.Osu
                         Object = lines.Skip(i).First(),
                         //Slider = false,
                         Banana = true,
-                        BananaShowerTime = new List<double>(),
-                        BananaShowerXOffset = new List<double>()
+                        BananaStart = Int32.Parse(amount[2]),
+                        BananaEnd = Int32.Parse(amount[5])
                     });
-                } else if (amount.Length > 7)
+                }/* else if (amount.Length > 7)
                 {
                     // Checks for sliders; they use the rng class as well like spinners
                     // According to https://osu.ppy.sh/wiki/en/osu%21_File_Formats/Osu_%28file_format%29#sliders; they are pretty dynamic whereas hitobjects and spinners are static, so I hope this line of code doesn't break anything with other beatmaps
                     AllHitObjects.Add(new GetObjectInfo
                     {
                         Object = lines.Skip(i).First(),
-                        //Slider = true,
+                        Slider = true,
                         // The line of code below is broken; this is because I lack knowledge on how sliders works and they are really confusing to work with
                         //SliderInheritedPoint = sp.GoingThroughTimingPoints(lines.Skip(i).First()[2], MusicInfo.GetItemLine("[TimingPoints]", lines) + 1, true, lines),
                         //SliderUninheritedPoint = sp.GoingThroughTimingPoints(lines.Skip(i).First()[2], MusicInfo.GetItemLine("[TimingPoints]", lines) + 1, false, lines),
                         Banana = false
                     });
-                }
+                }*/
                 else
                 {
                     // Normal objects added
@@ -69,79 +73,108 @@ namespace BananaPredictor.Osu
                 }
             }
 
-            // Processing each spinner - The logic for generating the time interval for each banana according to the catch rulesets; all rights go to peppy and his mathematics, just using the important code
-            // Used according to https://github.com/ppy/osu/blob/master/osu.Game.Rulesets.Catch/Objects/BananaShower.cs
-            foreach (var item in AllHitObjects)
+            int num = AllHitObjects.Count;
+            for (int i = num - 1; i >= 0; i--)
             {
-                switch (item.Banana)
+                if (AllHitObjects[i].Banana)
                 {
-                    case true:
-                        String[] getitem = item.Object.Split(",");
-                        double time = Int32.Parse(getitem[2]);
-                        double endtime = Int32.Parse(getitem[5]);
-                        int duration = Int32.Parse(getitem[5]) - Int32.Parse(getitem[2]);
-
-                        double spacing = duration;
-                        while (spacing > 100)
-                            spacing /= 2;
-                        if (spacing <= 0)
-                            continue;
-
-                        while (time <= endtime)
+                    for (int j = AllHitObjects[i].BananaStart; j < AllHitObjects[i].BananaEnd - 1; j++)
+                    {
+                        AllHitObjects.Add(new GetObjectInfo
                         {
-                            item.BananaShowerTime.Add(time);
+                            Object = "256,192," + j + ",12,0," + ++j + ",0:0:0:0:",
+                            Banana = true,
+                            BananaStart = j,
+                            BananaEnd = j + 1,
+                            BananaShowerXOffset = new(),
+                            BananaShowerTime = new()
+                        });
+                    }
 
-                            time += spacing;
-                        }
-                        break;
+                    AllHitObjects.RemoveAt(i);
                 }
             }
+
+            // Processing each spinner - The logic for generating the time interval for each banana according to the catch rulesets; all rights go to peppy and his mathematics, just using the important code
+            // Used according to https://github.com/ppy/osu/blob/master/osu.Game.Rulesets.Catch/Objects/BananaShower.cs
+            foreach (var obj in AllHitObjects)
+            {
+                if (obj.Banana)
+                {
+                    String[] getitem = obj.Object.Split(",");
+                    double time = Int32.Parse(getitem[2]);
+                    double endtime = Int32.Parse(getitem[5]);
+                    int duration = Int32.Parse(getitem[5]) - Int32.Parse(getitem[2]);
+
+                    double spacing = duration;
+                    while (spacing > 100)
+                        spacing /= 2;
+                    if (spacing <= 0)
+                        continue;
+
+                    while (time <= endtime)
+                    {
+                        obj.BananaShowerTime.Add(time);
+
+                        time += spacing;
+                    }
+                }
+            }
+
+            // Mergesort
+            MergeSort ms = new();
+            AllHitObjects = ms.Merge(AllHitObjects);
+
+            if (AllHitObjects.Equals(null))
+                return false;
 
             // How each banana is processed - The logic the banana xoffset according to the catch rulesets; all rights go to peppy and his mathematics, just using the important code
             // Used according to https://github.com/ppy/osu/blob/master/osu.Game.Rulesets.Catch/Beatmaps/CatchBeatmapProcessor.cs
             var rng = new FastRandom(CatchBeatmapProcessor.RNG_SEED); // Why is the seed 1337?
             var temp = new FastRandom(CatchBeatmapProcessor.RNG_SEED);
-            bool wentwrong = false;
-            foreach (var obj in AllHitObjects)
+            int indx = 0;
+            while (indx < AllHitObjects.Count)
             {
-                if (obj.Banana)
+                System.Diagnostics.Debug.WriteLine("Processing {0} in indx {1}", AllHitObjects[indx].Object, indx);
+                if (AllHitObjects[indx].Banana)
                 {
-                    for (int i = 0; i < obj.BananaShowerTime.Count; i++)
+                    for (int i = 0; i < AllHitObjects[indx].BananaShowerTime.Count; i++)
                     {
+                        temp = rng;
                         double xOffSetCheck = (float)(rng.NextDouble() * CatchPlayfield.WIDTH);
-                        if (xOffSetCheck < startPoint && xOffSetCheck > endPoint)     // TODO: Figure out why this isn't working
+                        System.Diagnostics.Debug.WriteLine("Banana in {0} with {1}", AllHitObjects[indx].BananaShowerTime[i], xOffSetCheck);
+                        if (xOffSetCheck < startPoint || xOffSetCheck > endPoint)     // TODO: Figure out why this isn't working
                         {
+                            AllHitObjects.Insert(indx, new GetObjectInfo
+                            {
+                                Object = "256,144," + AllHitObjects[indx].BananaShowerTime[i] + ",6,0,L|256:166,1,20",
+                                Banana = false
+                            });
                             rng = temp;
-                            obj.BananaShowerXOffset.Clear();
-                            wentwrong = true;
+                            //indx = 0;
+                            AllHitObjects = ms.Merge(AllHitObjects);
                             break;
                         }
-                        obj.BananaShowerXOffset.Add(xOffSetCheck);
                         rng.Next();
                         rng.Next();
                         rng.Next();
                     }
-                    if (!wentwrong)
-                    {
-                        for (int i = 0; i < obj.BananaShowerXOffset.Count; i++)
-                        {
-                            temp.NextDouble();
-                            temp.Next();
-                            temp.Next();
-                            temp.Next();
-                        }
-                    }
+                    indx++;
+                } else
+                {
+                    rng.Next();
+                    indx++;
                 }
                 // TODO: Need to code in how it works
                 //else if (obj.Slider)
                 //{
-                    //foreach(var item in obj.SliderList)
-                    //{
-                        //else if (item.TinyDroplet)
-                            //rng.Next(20, 20);
-                        //else if (item.Droplet)
-                            //rng.Next();
-                    //}
+                //foreach(var item in obj.SliderList)
+                //{
+                //else if (item.TinyDroplet)
+                //rng.Next(20, 20);
+                //else if (item.Droplet)
+                //rng.Next();
+                //}
                 //}
             }
 
