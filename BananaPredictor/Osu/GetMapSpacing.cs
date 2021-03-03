2dadd591aada447a5e661f28c47bb63a65058740
KEVINJSCHURMAN <kevinjschurman@gmail.com>
2021-03-04 00:20:10 +0100

    Further minor edits and added todo

---
+++ BananaPredictor/Osu/GetMapSpacing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananaPredictor.Osu
+{
+    // TODO: Do this for spacing to prevent lag
+    public class GetMapSpacing
+    {
+        public int Spacing(String spinnerStart, IEnumerable<String> Path)
+        {
+            GetMusicInfo gmi = new();
+            int store = gmi.GetItemLine("[TimingPoints]", Path) + 1;
+
+            //int minimum = 0;
+            //int maximum = 0;
+
+            for (int i = store; i < Path.Count(); i++)
+            {
+                //if ()
+                //return i;
+            }
+
+            return -1;
+        }
+    }
+}
