65bae207b83dd2fc881be188091c546829c3ab1b
Kevinbestkiller KBK <kevinbestkiller@gmail.com>
2021-03-03 17:01:12 +0100

    Removed sliders for the time being since they were the biggest barrier to overcome. Testing only with drops (not droplets). Will later change again when everything starts going well again

--- BananaPredictor/Osu/GetObjectInfo.cs
+++ BananaPredictor/Osu/GetObjectInfo.cs
@@ -6,11 +6,14 @@ namespace BananaPredictor.Osu
     public class GetObjectInfo
     {
         public String Object { get; set; }
-        public bool Slider { get; set; }
-        public double SliderInheritedPoint { get; set; }
-        public double SliderUninheritedPoint { get; set; }
+        //public bool Slider { get; set; }
+        //public double SliderInheritedPoint { get; set; }
+        //public double SliderUninheritedPoint { get; set; }
         public bool Banana { get; set; }
         public List<double> BananaShowerTime { get; set; }
         public List<double> BananaShowerXOffset { get; set; }
+        // Testing
+        public int BananaStart { get; set; }
+        public int BananaEnd { get; set; }
     }
 }
