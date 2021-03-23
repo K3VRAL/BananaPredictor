bfdec8f7a9b869ff98c716839d77e6aa27a741ef
Kevinbestkiller KBK <kevinbestkiller@gmail.com>
2021-03-23 18:16:22 +0100

    Many edits to make dynamic more compatible

--- BananaPredictor/WinOptions.cs
+++ BananaPredictor/WinOptions.cs
@@ -31,7 +31,14 @@ namespace BananaPredictor
             tbLeftPos.BackColor = SystemColors.Control;
             tbRightPos.BackColor = SystemColors.Control;
 
+            // TODO: Get saved items
+            //if (win.bspr.spinnerSpecs == null) AddMore(); else spinnerAOR = win.bspr.spinnerSpecs;
             AddMore();
+
+            tbLeftStart.Text = "0";
+            tbRightStart.Text = "0";
+            tbLeftEnd.Text = "1";
+            tbRightEnd.Text = "1";
         }
 
         private K3BananaWindow win = null;
@@ -51,43 +58,40 @@ namespace BananaPredictor
 
         private void BSave_Click(object sender, EventArgs e)
         {
-            // TODO: Use this is SavingData() foreach item
-            if (!cbOnlySpin.Checked
-                && (String.IsNullOrEmpty(tbStartTime.Text) || String.IsNullOrWhiteSpace(tbStartTime.Text))
-                && (String.IsNullOrEmpty(tbEndTime.Text) || String.IsNullOrWhiteSpace(tbEndTime.Text))
-                && (String.IsNullOrEmpty(tbDistance.Text) || String.IsNullOrWhiteSpace(tbDistance.Text))
-                && (String.IsNullOrEmpty(tbLeftPos.Text) || String.IsNullOrWhiteSpace(tbLeftPos.Text))
-                && (String.IsNullOrEmpty(tbRightPos.Text) || String.IsNullOrWhiteSpace(tbRightPos.Text)))
-            {
-                Console.WriteLine("One, few or all lines are missing mandatory input");
-                MessageBox.Show("Input integers into the textboxes", "Error");
-                return;
-            }
-
-            try
-            {
-                if ((Int32.Parse(tbStartTime.Text) >= Int32.Parse(tbEndTime.Text))
-                    || (Int32.Parse(tbLeftPos.Text) >= Int32.Parse(tbRightPos.Text)))
-                {
-                    Console.WriteLine("Start Time or Pos is bigger than End Time or Pos");
-                    MessageBox.Show("Start Time or Pos must be bigger than End Time or Poss", "Error");
-                    return;
-                }
-                else if (Int32.Parse(tbDistance.Text) <= 0)
-                {
-                    Console.WriteLine("Distance Snap must be bigger than 0");
-                    MessageBox.Show("Distance Snap must be bigger than 0", "Error");
-                    return;
-                }
-            } catch (FormatException)
-            {
-                ExceptionMade();
-                return;
-            }
-
             SavingData(prev);
 
-            win.bspr.spinnerSpecs = spinnerAOR;
+            for (int i = 0; i < spinnerAOR.Count; i++)
+                if (spinnerAOR[i].MapRelated[0] >= spinnerAOR[i].MapRelated[1])
+                {
+                    Console.WriteLine("Start Time is bigger than End Time from list {0}", i);
+                    MessageBox.Show("Start Time is bigger than End Time from list" + i, "Error");
+                    cbList.SelectedItem = i;
+                    return;
+                }
+                else if (spinnerAOR[i].MapRelated[2] <= 0)
+                {
+                    Console.WriteLine("Distance Snap from list {0} must be bigger than 0", i);
+                    MessageBox.Show("Distance Snap from list " + i + " must be bigger than 0", "Error");
+                    cbList.SelectedItem = i;
+                    return;
+                }
+                else if (cbMoveTo.Checked && (spinnerAOR[i].SpinnerRelated[0] >= spinnerAOR[i].SpinnerRelated[2]
+                    || spinnerAOR[i].SpinnerRelated[1] >= spinnerAOR[i].SpinnerRelated[3]))
+                {
+                    Console.WriteLine("Left or Right Start is bigger than Left or Right End from list {0}. Use the Invert checkBox instead", i);
+                    MessageBox.Show("Left or Right Start is bigger than Left or Right End from list " + i + ". Use the Invert checkBox instead", "Error");
+                    cbList.SelectedItem = i;
+                    return;
+                }
+                else if (!cbMoveTo.Checked && spinnerAOR[i].SpinnerRelated[0] >= spinnerAOR[i].SpinnerRelated[1])
+                {
+                    Console.WriteLine("Left Empty Pos is bigger than Right Empty Pos from list {0}. Use the Invert checkBox instead", i);
+                    MessageBox.Show("Left Empty Pos is bigger than Right Empty Pos from list " + i + ". Use the Invert checkBox instead", "Error");
+                    cbList.SelectedItem = i;
+                    return;
+                }
+
+                    win.bspr.spinnerSpecs = spinnerAOR;
             win.lStatus.Text = "Values saved";
 
             MessageBox.Show("Values inputted", "Success");
@@ -100,13 +104,17 @@ namespace BananaPredictor
         // TODO: Replace this where it compliments the combobox
         private void BPlus_Click(object sender, EventArgs e)
         {
-            AddMore();
+            // TODO: Fix then remove this
+            MessageBox.Show("They are in a broken state right now; under development", "Warning");
+            //AddMore();
         }
 
         // TODO: Remove a list of spinners
         private void BMinus_Click(object sender, EventArgs e)
         {
-            int saving = (int)cbList.SelectedItem;
+            // TODO: Fix then remove this
+            MessageBox.Show("They are in a broken state right now; under development", "Warning");
+            /*int saving = (int)cbList.SelectedItem;
             if (cbList.Items.Count > 1)
             {
                 if ((int)cbList.SelectedIndex == 0)
@@ -127,7 +135,7 @@ namespace BananaPredictor
                 }
             }
             else
-                MessageBox.Show("You can only have a minimum amount of 1 spinner", "Error");
+                MessageBox.Show("You can only have a minimum amount of 1 spinner", "Error");*/
         }
 
         private void CbList_SelectedValueChanged(object sender, EventArgs e)
@@ -144,27 +152,37 @@ namespace BananaPredictor
         private void CbMoveTo_CheckedChanged(object sender, EventArgs e)
         {
             // TODO: Fix then remove this
-            if (cbMoveTo.Checked)
+            /*if (cbMoveTo.Checked)
                 MessageBox.Show("They are in a broken state right now; under development", "Warning");
-            cbMoveTo.Checked = false;
+            cbMoveTo.Checked = false;*/
 
-            /*if (!cbMoveTo.Checked)
+            if (!cbMoveTo.Checked)
             {
+                spinnerAOR[(int)cbList.SelectedItem].SpinnerRelated.RemoveAt(0);
+                spinnerAOR[(int)cbList.SelectedItem].SpinnerRelated.RemoveAt(0);
                 tbLeftStart.BackColor = Color.DarkGray;
                 tbRightStart.BackColor = Color.DarkGray;
                 tbLeftEnd.BackColor = Color.DarkGray;
                 tbRightEnd.BackColor = Color.DarkGray;
                 tbLeftPos.BackColor = SystemColors.Control;
                 tbRightPos.BackColor = SystemColors.Control;
+                tbLeftPos.Text = "0";
+                tbRightPos.Text = "1";
             }
             else
             {
+                spinnerAOR[(int)cbList.SelectedItem].SpinnerRelated.Add(0);
+                spinnerAOR[(int)cbList.SelectedItem].SpinnerRelated.Add(0);
                 tbLeftStart.BackColor = SystemColors.Control;
                 tbRightStart.BackColor = SystemColors.Control;
                 tbLeftEnd.BackColor = SystemColors.Control;
                 tbRightEnd.BackColor = SystemColors.Control;
                 tbLeftPos.BackColor = Color.DarkGray;
                 tbRightPos.BackColor = Color.DarkGray;
+                tbLeftStart.Text = "0";
+                tbRightStart.Text = "0";
+                tbLeftEnd.Text = "1";
+                tbRightEnd.Text = "1";
             }
 
             tbLeftStart.Enabled = cbMoveTo.Checked;
@@ -172,7 +190,7 @@ namespace BananaPredictor
             tbLeftEnd.Enabled = cbMoveTo.Checked;
             tbRightEnd.Enabled = cbMoveTo.Checked;
             tbLeftPos.Enabled = !cbMoveTo.Checked;
-            tbRightPos.Enabled = !cbMoveTo.Checked;*/
+            tbRightPos.Enabled = !cbMoveTo.Checked;
         }
 
         private void CbOnlySpin_CheckedChanged(object sender, EventArgs e)
@@ -182,28 +200,51 @@ namespace BananaPredictor
                 tbLeftPos.BackColor = Color.DarkGray;
                 tbRightPos.BackColor = Color.DarkGray;
 
+                tbLeftStart.BackColor = Color.DarkGray;
+                tbRightStart.BackColor = Color.DarkGray;
+                tbLeftEnd.BackColor = Color.DarkGray;
+                tbRightEnd.BackColor = Color.DarkGray;
+
                 tbLeftPos.Text = "0";
                 tbRightPos.Text = "1";
+
+                tbLeftStart.Text = "0";
+                tbRightStart.Text = "0";
+                tbLeftEnd.Text = "1";
+                tbRightEnd.Text = "1";
             }
             else
             {
-                tbLeftPos.BackColor = SystemColors.Control;
-                tbRightPos.BackColor = SystemColors.Control;
+                if (cbMoveTo.Checked)
+                {
+                    tbLeftStart.BackColor = SystemColors.Control;
+                    tbRightStart.BackColor = SystemColors.Control;
+                    tbLeftEnd.BackColor = SystemColors.Control;
+                    tbRightEnd.BackColor = SystemColors.Control;
+
+                    tbLeftStart.Enabled = !cbOnlySpin.Checked;
+                    tbRightStart.Enabled = !cbOnlySpin.Checked;
+                    tbLeftEnd.Enabled = !cbOnlySpin.Checked;
+                    tbRightEnd.Enabled = !cbOnlySpin.Checked;
+                }
+                else
+                {
+                    tbLeftPos.BackColor = SystemColors.Control;
+                    tbRightPos.BackColor = SystemColors.Control;
+
+                    tbLeftPos.Enabled = !cbOnlySpin.Checked;
+                    tbRightPos.Enabled = !cbOnlySpin.Checked;
+                }
             }
 
             cbInvert.Visible = !cbOnlySpin.Checked;
-            cbInvert.Enabled = !cbOnlySpin.Checked;
             cbMoveTo.Visible = !cbOnlySpin.Checked;
-            cbMoveTo.Enabled = !cbOnlySpin.Checked;
-
-            tbLeftPos.Enabled = !cbOnlySpin.Checked;
-            tbRightPos.Enabled = !cbOnlySpin.Checked;
         }
 
         // Functions to get things done easier
         private void AddMore()
         {
-            int num = spinnerAOR.Count == 0 ? 0 : spinnerAOR.Count;
+            int num = spinnerAOR.Count;
 
             cbList.Items.Add(num);
             // Instead of using int[], use List<int> to allow for more dynamic things
@@ -211,12 +252,12 @@ namespace BananaPredictor
             List<int> listMap = new();
             List<int> listSpin = new();
             listMap.Add(0);
-            listMap.Add(0);
+            listMap.Add(1);
             listMap.Add(1);
             listMap.Add(0);
             listMap.Add(0);
             listSpin.Add(0);
-            listSpin.Add(0);
+            listSpin.Add(1);
             spinnerAOR.Add(num, listMap, listSpin);
 
             cbList.SelectedItem = num;
@@ -228,29 +269,25 @@ namespace BananaPredictor
             try
             {
                 Console.WriteLine("Saving...");
-                /*if (cbMoveTo.Checked)
-                {
-                    spinnerAOR[num][0] = Int32.Parse(tbStartTime.Text);
-                    spinnerAOR[num][1] = Int32.Parse(tbEndTime.Text);
-                
-                    spinnerAOR[num][2] = Int32.Parse(tbEndTime.Text);
-                    spinnerAOR[num][3] = Convert.ToInt32(cbInvert.Checked);
 
-                    spinnerAOR[num][4] = Int32.Parse(tbLeftStart.Text);
-                    spinnerAOR[num][5] = Int32.Parse(tbRightStart.Text);
-                    spinnerAOR[num][6] = Int32.Parse(tbLeftEnd.Text);
-                    spinnerAOR[num][7] = Int32.Parse(tbRightEnd.Text);
-                }
-                else
-                {*/
                 spinnerAOR[num].MapRelated[0] = Int32.Parse(tbStartTime.Text);
                 spinnerAOR[num].MapRelated[1] = Int32.Parse(tbEndTime.Text);
                 spinnerAOR[num].MapRelated[2] = Int32.Parse(tbDistance.Text);
                 spinnerAOR[num].MapRelated[3] = Convert.ToInt32(cbInvert.Checked);
                 spinnerAOR[num].MapRelated[4] = Convert.ToInt32(cbOnlySpin.Checked);
 
-                spinnerAOR[num].SpinnerRelated[0] = Int32.Parse(tbLeftPos.Text);
-                spinnerAOR[num].SpinnerRelated[1] = Int32.Parse(tbRightPos.Text);
+                if (cbMoveTo.Checked)
+                {
+                    spinnerAOR[num].SpinnerRelated[0] = Int32.Parse(tbLeftStart.Text);
+                    spinnerAOR[num].SpinnerRelated[1] = Int32.Parse(tbRightStart.Text);
+                    spinnerAOR[num].SpinnerRelated[2] = Int32.Parse(tbLeftEnd.Text);
+                    spinnerAOR[num].SpinnerRelated[3] = Int32.Parse(tbRightEnd.Text);
+                }
+                else
+                {
+                    spinnerAOR[num].SpinnerRelated[0] = Int32.Parse(tbLeftPos.Text);
+                    spinnerAOR[num].SpinnerRelated[1] = Int32.Parse(tbRightPos.Text);
+                }
 
                 ConsoleSave();
             }
@@ -261,27 +298,45 @@ namespace BananaPredictor
             }
         }
 
-        private void ToTextBox(int number)
+        private void ToTextBox(int num)
         {
-            tbStartTime.Text = spinnerAOR[number].MapRelated[0].ToString();
-            tbEndTime.Text = spinnerAOR[number].MapRelated[1].ToString();
-            tbDistance.Text = spinnerAOR[number].MapRelated[2].ToString();
-            cbInvert.Checked = Convert.ToBoolean(spinnerAOR[number].MapRelated[3]);
-            cbOnlySpin.Checked = Convert.ToBoolean(spinnerAOR[number].MapRelated[4]);
+            tbStartTime.Text = spinnerAOR[num].MapRelated[0].ToString();
+            tbEndTime.Text = spinnerAOR[num].MapRelated[1].ToString();
+            tbDistance.Text = spinnerAOR[num].MapRelated[2].ToString();
+            cbInvert.Checked = Convert.ToBoolean(spinnerAOR[num].MapRelated[3]);
+            cbOnlySpin.Checked = Convert.ToBoolean(spinnerAOR[num].MapRelated[4]);
 
-            tbLeftPos.Text = spinnerAOR[number].SpinnerRelated[0].ToString();
-            tbRightPos.Text = spinnerAOR[number].SpinnerRelated[1].ToString();
+            if (cbMoveTo.Checked)
+            {
+                tbLeftStart.Text = spinnerAOR[num].SpinnerRelated[0].ToString();
+                tbRightStart.Text = spinnerAOR[num].SpinnerRelated[1].ToString();
+                tbLeftEnd.Text = spinnerAOR[num].SpinnerRelated[2].ToString();
+                tbRightEnd.Text = spinnerAOR[num].SpinnerRelated[3].ToString();
+            }
+            else
+            {
+                tbLeftPos.Text = spinnerAOR[num].SpinnerRelated[0].ToString();
+                tbRightPos.Text = spinnerAOR[num].SpinnerRelated[1].ToString();
+            }
         }
 
         private void ConsoleSave()
         {
             foreach (KeyValuePair<int, StoringInfo> kvp in spinnerAOR)
-                Console.WriteLine("Index: {0}"
-                    + "\nStartTime: {1},  EndTime: {2}, Distance: {3}, Invert: {4}, SpinnerOnly: {5},"
-                    + "\nStartPos: {6},  EndPos: {7}",
-                    kvp.Key,
-                    kvp.Value.MapRelated[0], kvp.Value.MapRelated[1], kvp.Value.MapRelated[2], Convert.ToBoolean(kvp.Value.MapRelated[3]), Convert.ToBoolean(kvp.Value.MapRelated[4]),
-                    kvp.Value.SpinnerRelated[0], kvp.Value.SpinnerRelated[1]);
+                if (kvp.Value.SpinnerRelated.Count == 2)
+                    Console.WriteLine("Index: {0}"
+                        + "\nStartTime: {1},  EndTime: {2}, Distance: {3}, Invert: {4}, SpinnerOnly: {5},"
+                        + "\nStartPos: {6},  EndPos: {7}",
+                        kvp.Key,
+                        kvp.Value.MapRelated[0], kvp.Value.MapRelated[1], kvp.Value.MapRelated[2], Convert.ToBoolean(kvp.Value.MapRelated[3]), Convert.ToBoolean(kvp.Value.MapRelated[4]),
+                        kvp.Value.SpinnerRelated[0], kvp.Value.SpinnerRelated[1]);
+                else if (kvp.Value.SpinnerRelated.Count == 4)
+                    Console.WriteLine("Index: {0}"
+                        + "\nStartTime: {1},  EndTime: {2}, Distance: {3}, Invert: {4}, SpinnerOnly: {5},"
+                        + "\nLeftStartPos: {6},  RightStartPos: {7},  LeftEndPos: {8},  RightEndPos: {9}",
+                        kvp.Key,
+                        kvp.Value.MapRelated[0], kvp.Value.MapRelated[1], kvp.Value.MapRelated[2], Convert.ToBoolean(kvp.Value.MapRelated[3]), Convert.ToBoolean(kvp.Value.MapRelated[4]),
+                        kvp.Value.SpinnerRelated[0], kvp.Value.SpinnerRelated[1], kvp.Value.SpinnerRelated[2], kvp.Value.SpinnerRelated[3]);
         }
 
         private void ExceptionMade()
@@ -290,11 +345,19 @@ namespace BananaPredictor
             tbStartTime.Text = "0";
             tbEndTime.Text = "0";
             tbDistance.Text = "1";
-            cbInvert.Checked = false;
-            cbOnlySpin.Checked = false;
 
-            tbLeftPos.Text = "0";
-            tbRightPos.Text = "0";
+            if (cbMoveTo.Checked)
+            {
+                tbLeftStart.Text = "0";
+                tbRightStart.Text = "0";
+                tbLeftEnd.Text = "1";
+                tbRightEnd.Text = "1";
+            }
+            else
+            {
+                tbLeftPos.Text = "0";
+                tbRightPos.Text = "1";
+            }
         }
 
         // Inputting integers only in TextBox
